0b3bdcd3b4a2335264cbffab033cc30b32a48902
root <krishnad@varnatech.com>
2015-12-22 21:36:29 +0530

    Included Local Settings Override

---
+++ guestfaculty/urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls import patterns,include, url
+from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.contrib.auth import views as auth_views
+from facultyapp import views
+
+
+from facultyapp.admin import gf_app_site
+
+
+
+urlpatterns = [
+    #url(r'^$', include('facultyapp.urls')),
+	#url(r'^admin_tools/', include('admin_tools.urls')),
+	#url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+	#url(r'^jet/', include('jet.urls', 'jet')), #jet theme URLS
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^faculty/', include('facultyapp.urls')),
+    #url(r'^planning/', include('gfplan.urls')),
+    #url(r'^application/$', lambda x: HttpResponseRedirect('/application/facultyapp/guestfacultycandidate/')),  # Redirect this to applications page directly
+    url(r'^application/', include(gf_app_site.urls)),
+    url(r'^application/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^application/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),	
+    url(r'^registration/', include('allauth.urls')),
+    #url(r'', include('model_report.urls')),
+    #url(r'^report_builder/', include('report_builder.urls')),
+    #url(r'^reporting/', include('reporting.urls')),
+    #url(r'^reports/', include('reportengine.urls')),
+]
+
+admin.site.site_header = 'Guest Faculty Administration'
