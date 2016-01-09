56ec2d9cce36d3767e6c8aaa5ebd534497ef60f5
root <krishnad@varnatech.com>
2016-01-09 11:27:41 +0530

    Modified Settings

--- guestfaculty/settings.py
+++ guestfaculty/settings.py
@@ -110,7 +110,7 @@ TEMPLATES = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.RemoteUserBackend',
+    #'django.contrib.auth.backends.RemoteUserBackend',
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
