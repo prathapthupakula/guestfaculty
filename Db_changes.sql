03804db62d3d5c1a51d18cee3e7a91d121da43b3
root <krishnad@varnatech.com>
2016-01-17 21:39:12 +0530

    DB Changes

--- Db_changes.sql
+++ Db_changes.sql
@@ -1,3 +1,2 @@
---- Removed constraints of Program/Location/Status from this
 ALTER TABLE  `gfaculty`.`guest_faculty_course_offer` DROP INDEX  `un_course_location_semester1_idx` ,
 ADD UNIQUE  `un_course_location_semester1_idx` (  `course_id` ,  `guest_faculty_id` ,  `semester_id` )
